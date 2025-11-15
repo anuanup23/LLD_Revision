a4bfdb2f7bba9251daf7d4dfda5d9ebe08d23778
ANUP KUMAR <anuanupkumar0@gmail.com>
2025-11-15 10:42:31 +0530

    Improvements in lock by key

--- ConcurrencyExperiments/src/blockVsWait/AccessWorker.java
+++ ConcurrencyExperiments/src/blockVsWait/AccessWorker.java
@@ -13,20 +13,53 @@ public class AccessWorker implements Callable<DummyObjectWithVersion> {
         this.accessLock = accessLock;
     }
 
-    /**
-     * Computes a result, or throws an exception if unable to do so.
-     *
-     * @return computed result
-     * @throws Exception if unable to compute a result
-     */
     @Override
     public DummyObjectWithVersion call() throws Exception {
-        accessLock.lock(dummyObject);
-        dummyObject.incrementVersion();
-        AtomicInteger currentThreadCount = WaitVsBlockRunner.threadCount.getOrDefault(Thread.currentThread().getName(), new AtomicInteger(0));
-        WaitVsBlockRunner.threadCount.put(Thread.currentThread().getName(), new AtomicInteger(currentThreadCount.incrementAndGet()));
-        Thread.sleep(200);
-        accessLock.unlock(dummyObject);
-        return this.dummyObject;
+
+        try {
+            accessLock.lock(dummyObject);
+
+            // business logic
+            dummyObject.incrementVersion();
+
+            AtomicInteger currentThreadCount =
+                    WaitVsBlockRunner.threadCount.getOrDefault(
+                            Thread.currentThread().getName(),
+                            new AtomicInteger(0)
+                    );
+
+            WaitVsBlockRunner.threadCount.put(
+                    Thread.currentThread().getName(),
+                    new AtomicInteger(currentThreadCount.incrementAndGet())
+            );
+
+            Thread.sleep(200);
+
+            return this.dummyObject;
+
+        } catch (Exception e) {
+            // EARLY LOGGING
+            System.err.println(
+                    "ERROR in AccessWorker (thread=" + Thread.currentThread().getName()
+                            + ", key=" + dummyObject + "): " + e
+            );
+            e.printStackTrace(System.err);
+
+            // rethrow so Future.get() also sees it
+            throw e;
+
+        } finally {
+            // ALWAYS unlock even if exception happens
+            try {
+                accessLock.unlock(dummyObject);
+            } catch (Exception unlockError) {
+                System.err.println(
+                        "UNLOCK FAILURE in thread=" + Thread.currentThread().getName()
+                                + " for key=" + dummyObject + ": " + unlockError
+                );
+                unlockError.printStackTrace(System.err);
+                throw unlockError;
+            }
+        }
     }
 }
